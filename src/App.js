e4f78cca6a19054a998096f00970d36e191350ed
RAHUL SINGH  CHAUHAN <buky6180@gmail.com>
2023-08-31 18:02:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss'
+import React from 'react';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Home from './components/Home'; // Import your Home component
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    
+  );
+};
+
+export default App;
